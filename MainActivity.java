2a0b3aec7e2c2636f1b157f051b70f9da1fa6ae0
Enockkk <44627709+Enockkk@users.noreply.github.com>
2019-12-14 14:13:05 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package cq.game.fivechess.game;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import cq.game.fivechess.R;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button fight = (Button) findViewById(R.id.fight);
+
+        fight.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FightGameActivity.class));
+            }
+        });
+    }
+
+}
